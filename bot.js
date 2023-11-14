0f512542937e3703b3e5f5910e757dba272fcd6b
Bar Einav <bareinav10@gmail.com>
2023-11-14 19:27:07 +0200

    Log In

--- bot.js
+++ bot.js
@@ -4,5 +4,11 @@ import { config } from "dotenv";
 config();
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds],
+	intents: [GatewayIntentBits.Guilds],
 });
+
+client.once(Events.ClientReady, () => {
+	console.log("ready\n" + client.user.tag);
+});
+
+client.login(process.env.TOKEN);
