6c961c920eed5c1a0478243bee23a8bc49d3f7de
Bar Einav <bareinav10@gmail.com>
2023-11-14 19:57:43 +0200

    Add interaction event and deploy commands on start

--- bot.js
+++ bot.js
@@ -1,14 +1,28 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import { config } from "dotenv";
+import { deploy } from "./deploy-commands.js";
+import * as hello from "./commands/hello.js";
 
 config();
+
+// Deploy Commands
+await deploy();
 
 const client = new Client({
 	intents: [GatewayIntentBits.Guilds],
 });
 
 client.once(Events.ClientReady, () => {
-	console.log("ready\n" + client.user.tag);
+	console.log("ready as:\n" + client.user.tag);
 });
 
 client.login(process.env.TOKEN);
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isCommand()) return;
+	switch (interaction.commandName) {
+		case "hello":
+			await hello.execute(interaction);
+			break;
+	}
+});
