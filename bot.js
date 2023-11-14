bd0fb0b9e9a13eaa472701097db4a9151d250154
Bar Einav <bareinav10@gmail.com>
2023-11-14 22:09:32 +0200

    Change hello to ping, and refactor command handling to work with infinite amount of commands

--- bot.js
+++ bot.js
@@ -1,28 +1,22 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
-import { config } from "dotenv";
 import { deploy } from "./deploy-commands.js";
-import * as hello from "./commands/hello.js";
+import { handleCommands } from "./handle-commands.js";
+import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+import { config } from "dotenv";
 
 config();
-
-// Deploy Commands
-await deploy();
 
 const client = new Client({
 	intents: [GatewayIntentBits.Guilds],
 });
 
-client.once(Events.ClientReady, () => {
-	console.log("ready as:\n" + client.user.tag);
+client.once(Events.ClientReady, (c) => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 client.login(process.env.TOKEN);
 
-client.on(Events.InteractionCreate, async (interaction) => {
-	if (!interaction.isCommand()) return;
-	switch (interaction.commandName) {
-	case "hello":
-		await hello.execute(interaction);
-		break;
-	}
-});
+client.commands = new Collection();
+
+await handleCommands(client);
+
+await deploy();
--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,9 @@
 import { SlashCommandBuilder } from "discord.js";
 
 export const data = new SlashCommandBuilder()
-	.setName("hello")
-	.setDescription("Prints 'Hello'");
+	.setName("ping")
+	.setDescription("Replies with Pong!");
 
 export async function execute(interaction) {
-	await interaction.reply("Hello");
-	console.log("hello");
+	await interaction.reply("Pong!");
 }
